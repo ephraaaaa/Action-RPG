490f50c02c2692fb9679884223a30167c4997094
Ephraim John A. Gadin <ephraimjohn.gadin@gmail.com>
2024-03-14 13:52:16 +0800

    Added timer to stop the moving object at certain point of time.

--- Enemies/bat.gd
+++ Enemies/bat.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 var knockback = Vector2.ZERO
-
+var knockback_speed = 100
 
 func _physics_process(delta):
 	velocity = knockback.move_toward(knockback, delta)
@@ -12,5 +12,8 @@ func _physics_process(delta):
 func _on_hurtbox_area_entered(area):
 	print("1 hit")
 	var direction = ( position - area.owner.position ).normalized()
-	knockback = direction * 100
-	#self.queue_free()
+	knockback = direction * 250
+	await get_tree().create_timer(.15).timeout
+	knockback = Vector2.ZERO
+	move_and_slide()
+	self.queue_free()
